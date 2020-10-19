7e26d2c82966b875ed65095072ec1fe3f1cb4614
Kei Sugano <kei.sugano@h-t.co.jp>
2020-10-19 17:13:43 +0900

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# jp-tax-office-list-scraper
+
+以下のサイトから、税務署の一覧をとってくるスクリプトです
+https://www.nta.go.jp/about/organization/access/map.htm#ichiran
+
+サイトの構造が更新されたらバグるとおもいます。（しりましぇぇん）
+
+もしその他不具合ある場合、chromedriver のバージョンやchrome のバージョンを確認してください。
---
+++ tax-scraper.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+url    = "https://www.nta.go.jp/about/organization/access/map.htm#ichiran"
+driver = webdriver.Chrome(executable_path='./chromedriver')
+driver.get(url)
+
+def get_content():
+    html = driver.find_element_by_tag_name('html') \
+                 .get_attribute('innerHTML')
+    return html
+
+def get_lines(html, kw, kw2=''):
+    tail  = "</a></td>"
+    lines = [x.replace(kw2, '')
+              .replace(tail, '')
+              .replace(' ', '')
+              .replace('\t', '') 
+             for x
+             in  html.split('\n') 
+             if  kw  in x and
+                 kw2 in x
+            ]
+    return lines
+
+html   = get_content()
+prefix = "<td style=\"border-right:none; border-left:none;\"><a href=\""
+lines  = get_lines(html, '/about/organization', prefix)
+
+def visit(link):
+    driver.get(link) 
+
+root = "https://www.nta.go.jp"
+
+for line in lines:
+    link = line.split("\">")[0]
+    visit(root + link)
+    _lines = get_lines(get_content(), '/about/organization')
+    for _line in filter(lambda x: 'pdf' not in x and 'location' in x and '<td><ahref' in x, _lines): 
+        lsp = _line.split("\">")
+        if(len(lsp) > 1):
+            print(lsp[1])
+        else:
+            print('ERROR')
+
+input("Press any key:")
+driver.close()
