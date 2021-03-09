176f5b4641f2db0c700a52a332e122c9559183c4
Kei Sugano <kei.sugano@h-t.co.jp>
2021-03-09 15:26:08 +0900

    tax office update

--- tax-scraper.py
+++ tax-scraper.py
@@ -2,6 +2,7 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import sys
 
 url    = "https://www.nta.go.jp/about/organization/access/map.htm#ichiran"
 driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver')
@@ -18,33 +19,48 @@ def get_lines(html, kw, kw2=''):
               .replace(tail, '')
               .replace(' ', '')
               .replace('\t', '') 
+              .split("\">")
              for x
              in  html.split('\n') 
-             if  kw  in x and
-                 kw2 in x
+             if  kw  in x  and
+                 'location' in x and
+                 ('県' in x or '都' in x or '府' in x or '道' in x) and
+                 'ページ' not in x
+            ]
+    return [list(filter(lambda x: 'tds' not in x, line)) for line in lines]
+
+def get_lines2(html, kw, kw2=''):
+    tail  = "</a></td>"
+    lines = [x.replace(kw2, '')
+              .replace(tail, '')
+              .replace(' ', '')
+              .replace('\t', '') 
+             for x
+             in  html.split('\n') 
             ]
     return lines
 
 html   = get_content()
 prefix = "<td style=\"border-right:none; border-left:none;\"><a href=\""
-lines  = get_lines(html, '/about/organization', prefix)
+lines  = get_lines(html, '', prefix)
 
 def visit(link):
     driver.get(link) 
 
 root = "https://www.nta.go.jp"
 
+i = 1
 for line in lines:
-    link = line.split("\">")[0]
+    link = line[0].replace("<ahref=\"", "")
     visit(root + link)
-    _lines = get_lines(get_content(), '/about/organization')
+    _lines = get_lines2(get_content(), '/about/organization')
     for _line in filter(lambda x: 'pdf' not in x and 'location' in x and '<td><ahref' in x, _lines): 
         lsp = _line.split("\">")
         if(len(lsp) > 1):
-            
-            print(link.split('/')[-1].replace('.htm', '') + ' ' + lsp[1].replace("</a>", "").replace("</td>", ""))
+            print(f"P{i}" + '\t' + line[1] + '\t' + lsp[1].replace("</a>", "").replace("</td>", ""))
         else:
             print('ERROR')
+    i = i + 1
 
 input("Press any key:")
 driver.close()
